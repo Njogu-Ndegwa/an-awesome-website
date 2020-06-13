159781f1d5457acd3755dca5af716bf9a8c1466c
Njogu0001 <dennisndegwa001@gmail.com>
2020-06-13 19:17:23 +0300

    mockApi

---
+++ src/mockApi/index.js
@@ -0,0 +1,131 @@
+export default {
+    technicalSkills: [
+        {
+            id: 1,
+            stack: 'Git',
+            level: '90%'
+        },
+    
+        {
+            id: 2,
+            stack: 'Javascript/ReactJS',
+            level: '85%'
+        },
+    
+        {
+            id: 3,
+            stack: 'Javascript/VanillaJS',
+            level: '85%'
+        },
+    
+    
+        {
+            id: 4,
+            stack: 'HTML&CSS',
+            level: '80%'
+        },
+    
+    
+    ],
+
+    softSkills: [
+        {
+            attribute: 'Communication',
+            level: 95
+        },
+
+        {
+            attribute: 'Team Integration',
+            level: 90
+        },
+
+        {
+            attribute: 'Professionalism',
+            level: 85
+        },
+
+        {
+            attribute: 'Project Management',
+            level: 90
+        }
+    ],
+
+    frameworks: ['ReactJS', 'Vanilla JS', 'Node js', 'HTML', 'CSS'],
+    stacks: [
+        {
+            id: 1,
+            title: 'Api Development',
+            description: 'Proficient in building robust and scalable REST APIs. I have ample experience building APIs with Node js and Express js.'
+        },
+        {
+            id: 2,
+            title: 'Web Development',
+            description: 'Proficient in building professional web frontend with top frameworks like ReactJS and VanillaJS. I also have worked with ExpressJs for server side rendered applications'
+        },
+        {
+            id: 3,
+            title: 'UI Design',
+            description: 'Using the best design tools like AdobeXD and Sketch to deliver industry standard UI designs that can be adopted in production. I fancy design and enjoy making websites presentable'
+        },
+    ],
+    userInfo: {
+        pic:
+        'https://firebasestorage.googleapis.com/v0/b/mr-ndegwa.appspot.com/o/portfolio3.png?alt=media&token=39364310-073a-43c3-a34d-021bbee1ca7e',
+        adress: 'Nairobi, Kenya',
+        email: 'dennisndegwa57@gmail.com',
+        phone: '+254 795 635 364',
+        title: 'Software Engineer',
+        bio:
+        'I am a self-motivated and goal oriented individual with the ability to follow through with projects from inception to completion using analytical and problem-solving skills.'
+        
+    },
+
+    stories: {
+        expiriences: [
+            {
+                id: 4,
+                title: 'Freelance Developer',
+                timeline: 'May 2020 - Present',
+                company: 'Freelance',
+                description: `<strong>Responsibilities</strong> <br/>
+                <ul>
+                    <li>Work as a freelancer on various projects offered by clients who need websites that work and backend systems that perform well.</li>
+                </ul>`
+            },
+            {
+                id: 1,
+                title: 'Software Engineer',
+                timeline: 'Jul 2019 – May 2020 • 10 mos',
+                company: 'Ciphercom',
+                description: `<p>
+                        Ciphercom Technology Solutions is a company situated in Roysambu Kenya.
+                        It is Concerned with making various software eg Sacco Management Systems
+                        to support client needs
+                        </p><br/>
+                        <strong>Responsibilities</strong> <br/>
+                        <ul>
+                          <li>Ability to handle highly stressed situations and unexpected errors in an efficient manner.</li>
+                          <li>Attending weekly client meetings to discuss the specifics of the automated tests. Backlog negotiation and project estimations.</li>
+                          <li>Coaching and mentoring.</li>
+                          <li>Coding, testing and fixing programming errors.</li>
+                          <li>Conceptual, logical and physical database design.</li>
+                          <li>Conduct training for new developers.</li>
+                          <li>Creating and executing unit test cases.</li>
+                          <li>Fixing some script related bugs.</li>
+                          <li>Involved in the release and delivery process of the product.</li>
+                          <li>On call during downtime, assisted in mitigating several production emergencies. Use JavaScript stack for back end and front end applications. </li>
+                        </ul>`
+              }
+        ],
+
+        studies: [
+            {
+                id: 1,
+                title: 'Bachelors degree, Computer Science',
+                company: 'Jomo Kenyatta University of Agriculture and Technology',
+                timeline: '2017 - 2021',
+                description: 'Jkuat is a collegiate research university based in Juja, 36 km North East of Nairobi, along Nairobi-Thika Highway. It is one of the largest universities in Kenya. Although its history as an educational institution dates back to 1981, it did not become an independent university until 1994.'
+            }
+        ]
+    }
+}
